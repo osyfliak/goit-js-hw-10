9cefd24117d737ce401fd6b25c3b23383957009a
Osyfliak Ivan <osuflak22@gmail.com>
2023-01-12 17:22:23 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './css/styles.css';
 
-import './css/styles.css';
-import { fetchCountries } from './js/fetchCountries';
+import { fetchCountries } from './js/fetchCountries.js';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
 
