2016cdd40d15308536b93997d66baec3085e723b
Osyfliak Ivan <osuflak22@gmail.com>
2023-01-12 17:12:59 +0200

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,11 +1,20 @@
-export function fetchCountries(name) {
-  const url = 'https://restcountries.com/v3.1/name/';
-  const filter = '?fields=name,capital,population,flags,languages';
-  return fetch(`${url}${name}${filter}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
+// export function fetchCountries(name) {
+//   const url = 'https://restcountries.com/v3.1/name/';
+//   const filter = '?fields=name,capital,population,flags,languages';
+//   return fetch(`${url}${name}${filter}`).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
 
-    return response.json();
-  });
+//     return response.json();
+//   });
+// }
+
+const url = 'https://restcountries.com/v3.1/name/'
+const filter = 'fields=name,capital,population,flags,languages'
+export function fetchCountries(name) {
+	return fetch(`${url}${name}?${filter}`)
+		.then(response => response.json())
+		.catch(error => console.log(error))
 }
+
